51a2316a3dd97cb0b793c8da03da22c7ab6296ad
monwar23 <monwar2345@gmail.com>
2024-10-10 13:37:52 +0600

    complete project section

---
+++ src/components/Projects.jsx
@@ -0,0 +1,47 @@
+
+const Projects = () => {
+    return (
+        <section id="projects" className="py-16 font-poppins">
+        <div className="container mx-auto">
+        <h2 className="text-6xl font-semibold mb-6 text-center">Services</h2>
+        <p className="text-xl text-center mb-10">Lorem ipsum dolor sit amet consectetur. Mollis erat duis aliquam mauris est risus <br /> lectus. Phasellus consequat urna tellus</p> 
+          <div className="flex justify-center mb-8 space-x-4">
+            <button className=" border px-4 py-2 rounded text-xl hover:bg-gray-200">All</button>
+            <button className="border px-4 py-2 rounded hover:bg-gray-200 text-xl">UI/UX</button>
+            <button className="border px-4 py-2 rounded bg-orange-500 hover:bg-orange-600 text-xl text-white">Web Design</button>
+            <button className="border px-4 py-2 rounded hover:bg-gray-200 text-xl">App Design</button>
+            <button className="border px-4 py-2 rounded hover:bg-gray-200 text-xl">Graphic Design</button>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-5">
+            <div className="">
+              <div className="bg-[#FFEBDB]">
+              <img src="/public/Web Designs.png" alt="Project 1" className="mb-5 absolute mt-20 ml-4" />
+              <img src="/public/Web Designs (1).png" alt="Project 1" className="mb-4 ml-24" />
+              </div>
+              <p className="text-lg text-orange-500">Web Design</p>
+              <h3 className="text-xl font-bold">AirCalling Landing Page Design</h3>
+            </div>
+            <div className="">
+              <div className="bg-[#FFEBDB]">
+              <img src="/public/Web Designs (2).png" alt="Project 1" className="mb-5 absolute mt-20 ml-4" />
+              <img src="/public/Web Designs (3).png" alt="Project 1" className="mb-4 ml-24" />
+              </div>
+              <p className="text-lg text-orange-500">Web Design</p>
+              <h3 className="text-xl font-bold">Business Landing Page Design </h3>
+            </div>
+            <div className="">
+              <div className="bg-[#FFEBDB]">
+              <img src="public/Web Designs (4).png" alt="Project 1" className="mb-5 absolute mt-20 ml-4" />
+              <img src="public/Web Designs (5).png" alt="Project 1" className="mb-4 ml-24" />
+              </div>
+              <p className="text-lg text-orange-500">Web Design</p>
+              <h3 className="text-xl font-bold">Ecom Web Page Design </h3>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    );
+};
+
+export default Projects;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,29 +1,29 @@
 
 const Services = () => {
     return (
-        <section id="services" className=" py-16 px-5">
+        <section id="services" className=" py-16 px-5 font-poppins">
         <div className="container mx-auto">
           <h2 className="text-6xl font-semibold mb-6 text-center">Services</h2>
           <p className="text-xl text-center mb-10">Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa nibh lectus <br /> netus in. Aliquet donec morbi convallis pretium</p>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             <div className="bg-gray-100 p-6 shadow-lg rounded-lg">
               <img src="/public/1.png" alt="UI/UX" className=" h-16 w-16 mb-4" />
-              <h3 className="text-3xl font-semibold mb-2">UI/UX</h3>
+              <h3 className="text-2xl font-semibold mb-2">UI/UX</h3>
               <p className="text-lg">Lorem ipsum dolor sit amet consectetur. Morbi diam nisi<br /> nam diam interdum</p>
             </div>
             <div className="bg-gray-100 p-6 shadow-lg rounded-lg">
               <img src="/public/2.png" alt="Web Design" className="w-16 h-20 mb-4" />
-              <h3 className="text-3xl font-semibold mb-2">Web Design</h3>
+              <h3 className="text-2xl font-semibold mb-2">Web Design</h3>
               <p className="text-lg">Lorem ipsum dolor sit amet consectetur. Morbi diam nisi<br /> nam diam interdum</p>
             </div>
             <div className="bg-gray-100 p-6 shadow-lg rounded-lg">
               <img src="/public/3.png" alt="App Design" className=" w-12 h-20 mb-4" />
-              <h3 className="text-3xl font-semibold mb-2">App Design</h3>
+              <h3 className="text-2xl font-semibold mb-2">App Design</h3>
               <p className="text-lg">Lorem ipsum dolor sit amet consectetur. Morbi diam nisi<br /> nam diam interdum</p>
             </div>
             <div className="bg-gray-100 p-6 shadow-lg rounded-lg">
               <img src="/public/4.png" alt="Graphic Design" className=" h-16 w-20 mb-4" />
-              <h3 className="text-3xl font-semibold mb-2">Graphic Design</h3>
+              <h3 className="text-2xl font-semibold mb-2">Graphic Design</h3>
               <p className="text-lg">Lorem ipsum dolor sit amet consectetur. Morbi diam nisi<br /> nam diam interdum</p>
             </div>
           </div>
