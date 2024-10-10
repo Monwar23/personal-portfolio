cc98a37cb3fb22dcd9806fe95dad78c1b0148e46
monwar23 <monwar2345@gmail.com>
2024-10-10 14:44:16 +0600

    complete contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
@@ -14,6 +15,7 @@ function App() {
       <Services></Services>
       <Projects></Projects>
       <Testimonials></Testimonials>
+      <Contact></Contact>
 
     </>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,27 @@
+const Contact = () => {
+    return (
+        <section id="contact" className="py-16 font-poppins text-center">
+            <div className="container mx-auto">
+                <h2 className="text-6xl font-semibold mb-6">Lets Design Together</h2>
+                <p className="text-xl mb-10">
+                    Lorem ipsum dolor sit amet consectetur. Mollis erat duis aliquam mauris est risus <br />
+                    lectus. Phasellus consequat urna tellus.
+                </p>
+                <div className="max-w-3xl mx-auto flex flex-col md:flex-row gap-4">
+                    <input
+                        type="email"
+                        placeholder="Enter Your Email"
+                        className="border border-gray-300 p-4 rounded-xl flex-grow"
+                        aria-label="Email address"
+                        required
+                    />
+                    <button className="bg-orange-500 text-white p-4 rounded-xl text-lg font-semibold hover:bg-orange-600 flex-shrink-0">
+                        Contact Me
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
