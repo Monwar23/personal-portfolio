f914573fc3ee906f9801d781374efb8edd473ff1
monwar23 <monwar2345@gmail.com>
2024-10-10 12:54:58 +0600

    completed services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header></Header>
       <Hero></Hero>
       <About></About>
+      <Services></Services>
 
     </>
   );
